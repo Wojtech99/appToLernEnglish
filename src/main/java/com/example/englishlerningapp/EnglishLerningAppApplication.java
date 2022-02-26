280836ec907e3d80ad93f2b1d0dd2e509e4a132b
Wojciech <wojteknab@gmail.com>
2022-02-26 14:45:29 +0100

    Initial commit

---
+++ src/main/java/com/example/englishlerningapp/EnglishLerningAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.englishlerningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishLerningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishLerningAppApplication.class, args);
+    }
+
+}
